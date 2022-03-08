712362dc0da42d056b490e89dd010b3dd0d914d4
StevenMHernandez <me@stevenmhernandez.com>
2022-03-07 21:20:32 -0500

    add csipi_command tty-plugin to allow TTY microcontrollers to directly control CSI-Pi. For example, this allows an attached Arduino to enable and disable CSI-collection at-will without human interaction.

---
+++ src/csi_pi/tty_plugins/csipi_command_plugin.py
@@ -0,0 +1,49 @@
+import requests
+
+
+def get_object(tty_full_path, tty_save_path, experiment_name_file_path, config):
+    return CSIPiCommandTTYPlugin(tty_full_path, tty_save_path, experiment_name_file_path, config)
+
+
+class CSIPiCommandTTYPlugin:
+    """
+    This TTY Plugin handles CSI-Pi commands starting with `CSIPI_COMMAND,`
+
+    This allows your TTY microcontroller to automatically control CSI-Pi such as
+
+    `CSIPI_COMMAND,POWER_ON` which enable CSI data collection
+    `CSIPI_COMMAND,POWER_OFF` which disables CSI data collection
+
+    To "install" this plugin, add `src.csi_pi.tty_plugins.csipi_command_plugin` to the
+    `TTY_PLUGINS` variable within your `./env` file.
+    """
+    def __init__(self, tty_full_path, tty_save_path, experiment_name_file_path, config):
+        pass
+
+    def prefix_string(self):
+        """
+        For each incoming line from our devices we will look for the following prefix-string.
+        If the line starts with this string, then we will call `process(line)`.
+        :return: str
+        """
+
+        return "CSIPI_COMMAND,"
+
+    def process(self, line):
+        """
+        When we find a string that begins with `prefix_string()`, this function will be called.
+        Process the incoming line as you wish.
+
+        :param line: str
+        :return: None
+        """
+        if "POWER_ON" in line:
+            requests.post(f'http://localhost:8080/power_up')
+        elif "POWER_OFF" in line:
+            requests.post(f'http://localhost:8080/power_down')
+        else:
+            print(f"Unknown CSI_PI_COMMAND: '{line}'")
+
+    def process_every_second(self, current_second):
+        pass
+

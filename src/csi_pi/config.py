d1da8abab5b79ca4b16bc7acfcce6543b2192d88
StevenMHernandez <me@stevenmhernandez.com>
2022-03-15 10:10:43 -0400

    allow tty_plugins to be commented out with `#`

--- src/csi_pi/config.py
+++ src/csi_pi/config.py
@@ -10,7 +10,11 @@ class Config:
     esp32_baud_rate = int(os.environ.get('ESP32_BAUD_RATE', 921600))
     acm_baud_rate = int(os.environ.get('ACM_BAUD_RATE', 115200))
 
-    tty_plugins = os.environ.get('TTY_PLUGINS', "src.csi_pi.tty_plugins.csi_data_plugin").strip().split("\n")
+    tty_plugins = [
+        plugin for plugin in
+        os.environ.get('TTY_PLUGINS', "src.csi_pi.tty_plugins.csi_data_plugin").strip().split("\n")
+        if plugin[0] != "#" # any lines starting with `#` are comments and should be ignored
+    ]
 
     WRITE_CSI_LOCK = '/tmp/lock.write_csi.txt'
     data_dir = f"{app_dir}/storage/data/{time()}/"
